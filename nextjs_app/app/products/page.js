493eb4f92a75f5bc479e508a556206c9568af647
omzkiii <125126138+omzkiii@users.noreply.github.com>
2023-11-08 11:09:17 +0800

    Revert "main merge to trixia branch"

---
+++ nextjs_app/app/products/page.js
@@ -0,0 +1,42 @@
+// pages/product.js
+import React from 'react';
+
+const products = [
+  {
+    id: 1,
+    name: 'Kamote',
+    category: 'Vegetables',
+    description: 'Just a good kamote',
+    price: 1,
+  },
+  {
+    id: 2,
+    name: 'Chicken Breast',
+    category: 'Meat',
+    description: 'Tender that will make your heart surrender.',
+    price: 420.69,
+  },
+  // Add more product objects here...
+];
+
+const ProductPage = () => {
+  return (
+    <div className="bg-green-100 min-h-screen">
+      <div className="max-w-6xl mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {products.map((product) => (
+          <div key={product.id} className="bg-white rounded-lg shadow-lg p-4">
+            <h1 className="text-3xl font-bold text-green-700">{product.name}</h1>
+            <p className="text-green-600">Category: {product.category}</p>
+            <p className="text-green-800 text-lg mt-4">{product.description}</p>
+            <div className="mt-4 text-2xl text-green-700 font-semibold">PHP {product.price.toFixed(2)}</div>
+            <button className="bg-green-500 text-white font-semibold py-2 px-4 rounded-lg mt-4 hover:bg-green-600">
+              Add to Cart
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
--- project/app/serializers.py
+++ project/app/serializers.py
@@ -1,26 +1,7 @@
 from rest_framework import serializers
-from .models import Product
-from django.contrib.auth.models import User
+from .models import User
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
-
-class UserSignupSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150)
-    email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
-
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(write_only=True)
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+        fields = ('username', 'name')
