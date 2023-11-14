c97330bae6e694e41f832428f42648aa1d65c803
krishadn <116531561+krishadn@users.noreply.github.com>
2023-11-14 15:25:44 +0800

    Added some models

--- project/app/models.py
+++ project/app/models.py
@@ -1,14 +1,36 @@
 from django.db import models
+from django.contrib.auth import models as md
 
 # Create your models here.
 
+class Customer(md.User):
+    pass
+
     
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
+    fruit = "Fruit"
+    vegetable = "Vegetable"
+
+    CATEGORY = [
+        (fruit, fruit)
+        (vegetable, vegetable)
+    ]
+
+
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100)
+    unit = models.CharField(max_length=255)
+    quantity = models.DecimalField(max_digits=20, decimal_places=2) 
+    price = models.DecimalField(max_digits=20, decimal_places=2) #per unit
+    cost = models.DecimalField(max_digits=20, decimal_places=2) #per unit
+    category = models.CharField(max_length=100, choices=CATEGORY)
+    image = models.ImageField(upload_to="products/")
+    date_created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
+    
+
+class Order(models.Model):
+    pass
