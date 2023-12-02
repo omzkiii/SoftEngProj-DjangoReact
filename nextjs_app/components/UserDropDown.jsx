fd2e7f21cf916ce123f4b7e2726eb72d635b159d
krishadn <116531561+krishadn@users.noreply.github.com>
2023-12-02 11:09:58 +0800

    connected user profile page to backend

--- nextjs_app/components/UserDropDown.jsx
+++ nextjs_app/components/UserDropDown.jsx
@@ -4,7 +4,10 @@ import { useLoggedInContext } from '../contexts/LoggedInContext';
 
 
 const ProductDropDown = ({ onClick }) => {
-    const { logout } = useLoggedInContext();
+    const { user, logout } = useLoggedInContext();
+
+
+
     const handleLogout = async() =>{
       try {
         const response = await axios.post('http://127.0.0.1:8000/auth/token/logout/',{},{
@@ -27,7 +30,7 @@ const ProductDropDown = ({ onClick }) => {
     return (
         <>
             <ul className="absolute top-18 bg-green-600 border rounded shadow-lg">
-                    <Link href={`/profile/user`}><li onClick={onClick} className="px-4 py-2 cursor-pointer hover:bg-gray-100 hover:text-green-950">
+                    <Link href={`/profile/user/${user.username}`}><li onClick={onClick} className="px-4 py-2 cursor-pointer hover:bg-gray-100 hover:text-green-950">
                       Profile
                     </li></Link>
                     <li onClick={handleLogout} 
--- nextjs_app/contexts/LoggedInContext.jsx
+++ nextjs_app/contexts/LoggedInContext.jsx
@@ -1,25 +1,58 @@
 "use client"
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from "axios";
 
 const LoggedInContext = createContext();
 
 export const LoggedInProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [customer, setCustomer] = useState({})
 
   const login = () => {
     setIsLoggedIn(true);
+    getUser()
   };
 
   const logout = () => {
     setIsLoggedIn(false);
   };
+
+  const getUser = async () => {
+
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/auth/users/me/',{
+        headers: {
+          'Authorization': 'Token ' + localStorage.getItem('token')
+        },
+      })
+
+      const responseCust = await axios.get(`http://127.0.0.1:8000/auth/updatecustomer/${response.data.id}`,{
+        headers: {
+          'Authorization': 'Token ' + localStorage.getItem('token')
+        },
+      })
+
+      setUser(response.data)
+      setCustomer(responseCust.data)
+      console.log(response.data)
+      console.log(responseCust.data)
+
+
+    } catch (error) {
+      
+    }
+  }
+
+
   useEffect(() =>{
     if (localStorage.getItem('token')) {
         setIsLoggedIn(true)}
-  })
+        getUser()      
+  },[])
 
   return (
-    <LoggedInContext.Provider value={{ isLoggedIn, login, logout, setIsLoggedIn }}>
+    <LoggedInContext.Provider value={{ isLoggedIn, login, logout, setIsLoggedIn, user, customer }}>
       {children}
     </LoggedInContext.Provider>
   );
