0a43e192df2dcc1f4d5791a8d20391dac099e80a
dandy <mdreyes@tip.edu.ph>
2023-12-03 22:07:47 +0800

    idk

--- nextjs_app/app/checkout/page.jsx
+++ nextjs_app/app/checkout/page.jsx
@@ -6,82 +6,108 @@ const CheckOutPage = () => {
   return (
     <>
       <div className="h-screen w-screen bg-green-100">
-        
-          <div className="checkout-form h-screen w-screen bg-green-400 font-bold flex flex-col">
+        <div className="w-full flex flex-row">
+          <div className="h-screen w-screen bg-green-400 font-bold flex flex-col">
             <h2 class="font-sans m-8 text-4xl text-green-900">Checkout</h2>
 
-            <form class=" p-8 pt-0  rounded shadow-md">
+            <div class=" p-8 pt-0  rounded">
     
-              <div className="mb-2 flex ">
+              <div className="mb-2 flex justify-center">
                   <div className="field1 pr-10 w-1/2">
                     <label htmlFor="field1" className="block text-gray-700 text-sm font-bold mb-2">NAME</label>
-                    <input type="text" id="field1" name="field1" className="w-full p-2 border border-gray-300 rounded-[45px]" />
+                    <input type="text" id="field1" name="field1" className="w-full p-2 text-black border border-gray-300 rounded-[45px]" />
                   </div>
                     <div className="flex-1 w-1/2">
                     <label htmlFor="field2" className="block text-gray-700 text-sm font-bold mb-2">CONTACT NO.</label>
-                    <input type="text" id="field2" name="field2" className="w-full p-2 border border-gray-300 rounded-[45px]" />
+                    <input type="text" id="field2" name="field2" className="w-full p-2 text-black border border-gray-300 rounded-[45px]" />
                   </div>
               </div>
 
     
               <div class="mb-2">
                 <label for="field2" class="block text-gray-700 text-sm font-bold mb-2">ADDRESS</label>
-                <input type="text" id="field2" name="field2" class="w-full p-2 border border-gray-300 rounded-[45px]"></input>
+                <input type="text" id="field2" name="field2" class="w-full p-2 border border-gray-300 text-black rounded-[45px]"></input>
               </div>
-
-    
-              <div class="mb-2">
-                <label for="field3" class="block text-gray-700 text-sm font-bold mb-2">APPARTMENT,SUITE,FLOOR</label>
-                <input type="text" id="field3" name="field3" class="w-full p-2 border border-gray-300 rounded-[45px]"></input>
               </div>
-
-    
-            <div className="mb-2 flex gap-12">
-                <div className="flex-1">
-                  <label htmlFor="field3" className="block text-gray-700 text-sm font-bold mb-2">REGION</label>
-                  <input type="text" id="field3" name="field3" className="w-full p-2 border border-gray-300 rounded-[45px]" />
-                </div>
-                <div className="flex-1">
-                  <label htmlFor="field4" className="block text-gray-700 text-sm font-bold mb-2">CITY</label>
-                  <input type="text" id="field4" name="field4" className="w-full p-2 border border-gray-300 rounded-[45px]" />
-                </div>
-                <div className="flex-1">
-                  <label htmlFor="field5" className="block text-gray-700 text-sm font-bold mb-2">POSTAL CODE</label>
-                  <input type="text" id="field5" name="field5" className="w-full p-2 border border-gray-300 rounded-[45px]" />
-                </div>
-            </div>
 
               <h2 class="font-sans text-center mb-5 mt-8 text-4xl text-green-900">Payment</h2>
-
               <div className="mb-4 flex justify-center mx-auto font-sans font-bold gap-12"> 
-              <div className="w ">
-                <button type="button" id="button3" className="px-9 p-2 border border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+              <div className="">
+                <button type="radio" id="button3" className="px-9 p-3 border border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
                   GCASH
                 </button>
               </div>
               <div className="">
-                <button type="button" id="button4" className="px-8 py-2 border text-s border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+                <button type="radio" id="button4" className="px-9 p-3 border text-s border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
                   CREDIT/DEBIT CARD
                 </button>
               </div>
               <div className="">
-                <button type="button" id="button5" className="px-9 p-2 border border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+                <button type="radio" id="button5" className="px-9 p-3 border border-gray-300 rounded-[10px] bg-white text-green-900 hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
                   MAYA
                 </button>
               </div>
-            </div>
+              </div>
 
-
-
-            <div className="div flex justify-center">
-              <button type="submit" class="bg-green-900  text-white p-2  hover:bg-blue-700 w-2/3">PLACE ORDER</button>
-            </div>
-  </form>
-
+               <div className="div flex justify-center">
+              <button type="submit" class="bg-green-900  text-white p-2  hover:bg-blue-700 w-2/6  ">PLACE ORDER</button>
+             </div>
+             
             
-          </div>
+              </div>
+          {/* order summary */}
+            <div className="w-2/5  text-black justify-center flex-col bg-white ">
+            <h2 class="font-sans m-10 text-4xl font-semibold text-center text-green-900">Order Summary</h2>
+            <div className="flex flex-row mx-10 ">
+                <div class=" px-10 py-4 border-solid border-gray-200 text-center font-bold">
+                <img src="orangUserIcon.png" alt="Image Description" class="w-20 h-20 object-cover mx-auto"></img></div>
+                <div class="flex flex-col">
+              <h1 class=" border-solid border-gray-200 font-sans font-bold text-4xl text-green-90 ">ubat</h1>
+              <h1 class="px-full  border-solid  border-gray-200 font-sans font-semibold text-3xl text-green-900 ">P.500</h1>       
+                </div>
+            </div>
+
+            <div className="flex flex-row mx-10 ">
+                <div class=" px-10 py-4 border-solid border-gray-200 text-center font-bold">
+                <img src="orangeUserIcon.png" alt="Image Description" class="w-20 h-20 object-cover mx-auto"></img></div>
+                <div class="flex flex-col">
+              <h1 class=" border-solid border-gray-200 font-sans font-bold text-4xl text-green-90 ">ubat</h1>
+              <h1 class="px-full  border-solid  border-gray-200 font-sans font-semibold text-3xl text-green-900 ">P.500</h1>       
+                </div>
+            </div>
+
+            <div className="flex flex-row mx-10 ">
+                <div class=" px-10 py-4 border-solid border-gray-200 text-center font-bold">
+                <img src="logobg.png" alt="Image Description" class="w-20 h-20 object-cover mx-auto"></img></div>
+                <div class="flex flex-col">
+              <h1 class=" border-solid border-gray-200 font-sans font-bold text-4xl text-green-90 ">ubat</h1>
+              <h1 class="px-full  border-solid  border-gray-200 font-sans font-semibold text-3xl text-green-900 ">P.500</h1>       
+                </div>
+            </div>
+            {/* tatlo lng baka sumabog */}
+                    <div className=" flex flex-row justify-center text-xl text-green-900 font-sans font-semibold mx-10 mt-7">
+                      <div class="w-1/2">SUBTOTAL</div> 
+                      <div class="w-1/2">P1500.00</div>
+                    </div>
+
+                    <div className=" flex flex-row justify-center text-green-900 font-sans font-semibold mx-10 ">
+                      <div class="w-1/2">DISCOUNT</div> 
+                      <div class="w-1/2">P1000</div>
+                    </div>
+
+                    <div className=" flex flex-row justify-center text-orange-500 text-4xl font-sans font-bold mx-10">
+                      <div class="w-1/2">TOTAL</div>
+                      <div class="w-1/2">P2500</div>
+                    </div>
+            </div>
+
+            <div>
+
+            </div>
         
-      </div>
+            </div>
+
+          </div>
     </>
   );
 };
--- nextjs_app/app/usermanageinventory/page.jsx
+++ nextjs_app/app/usermanageinventory/page.jsx
@@ -11,7 +11,7 @@ const userinventory = () => {
         <div className="h-screen w-auto font-sans bg-green-100 text-lg flex-column">
         <div className="h-auto w-auto flex-column flex-start pt-5 pb-5 border-solid 2 border-orange-500 container mx-auto">
           <div class="w-max h-5">
-          <button class="bg-orange-500 text-black text-sm font-bold focus:ring-4 border-solid border border-l-orange-500 rounded-full px-5 py-1.5 text-center inline-flex items-center mr-2 dark:hover:bg-green-500 dark:hover:text-orange-500 dark:border-black">
+          <button class="bg-green-900 text-amber-400 text-sm font-bold focus:ring-4 border-solid border border-l-orange-500 rounded-full px-5 py-1.5 text-center inline-flex items-center mr-2 dark:hover:bg-green-900 dark:hover:text-orange-500 dark:border-black">
              <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
             VIEW ALL ORDERS
@@ -19,32 +19,32 @@ const userinventory = () => {
           </div>
         </div>
   
-        <div className="h-auto w-auto flex justify-space-between container mx-auto text-black">
-          <div class="h-auto flex justify-start font-semibold">
+        <div className="h-auto w-auto flex justify-space-between container mx-auto  text-black">
+          <div class="h-auto flex justify-start font-bold">
             <p>ORDER NUMBER: {}</p>
           </div>
         </div>
   
-        <div className="h-auto w-auto flex justify-space-between p-1 container mx-auto text-black">
+        <div className="h-auto w-auto flex justify-space-between p-1 font-bold container mx-auto  text-black">
           <div class="w-3/4">
             <h1 class="text-5xl">#69696969{}</h1>
           </div>
         </div>
        
-        <div className="h-auto w-auto flex justify-space-between p-1 text-black container mx-auto">
-          <div class="w-3/4 font-semibold">
+        <div className="h-auto w-auto flex justify-space-between p-1  text-green-800 container mx-auto">
+          <div class="w-3/4 font-bold">
             <h1 class>CUSTOMER: male{} </h1>
           </div>
-          <div class="w-1/4 flex jutify-end font-semibold">
+          <div class="w-1/4 flex jutify-end font-bold">
             <h1 class>CONTACT NO.: 09696969 {}</h1>
           </div>
         </div>
   
-        <div className="h-auto w-75 flex p-1 container mx-auto text-black">
-          <div class="w-3/4 font-semibold">
+        <div className="h-auto w-75 flex p-1 text-green-800 container mx-auto ">
+          <div class="w-3/4 font-bold">
             <h1 class>ADDRESS: {}bahay 123 purok 0233 bayan ng blablabla lungsod ng tokyo</h1>
           </div>
-          <div class="w-1/4 flex jutify-end font-semibold">
+          <div class="w-1/4 flex jutify-end text-green-800 font-bold ">
             <h1 class>PAYMENT: {}pera </h1>
           </div>
         </div>
@@ -59,7 +59,7 @@ const userinventory = () => {
         <col width="16.6667%"></col>
       </colgroup>
       <thead>
-        <tr className="bg-orange-400 text-white text-4xl">
+        <tr className="bg-green-900 text-white text-4xl">
           <th className="px-6 py-3 uppercase font-semi-bold text-sm border-solid border-gray-200 text-center"></th>
           <th className="px-6 py-3 uppercase font-semi-bold text-xl border-solid border-gray-200 text-center">Product</th>
           <th className="px-6 py-3 uppercase font-semi-bold text-xl border-solid border-gray-200 text-center">Quantity</th>
@@ -70,24 +70,24 @@ const userinventory = () => {
       <tr>
         <td class="px-6 py-4 border-solid border-gray-200 text-center font-semi-bold">
         <img src="logobg.png" alt="Image Description" class="w-16 h-16 object-cover mx-auto"></img></td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
     </tr>
 
     <tr>
         <td class="px-6 py-4 border-solid border-gray-200 text-center font-semi-bold">
         <img src="logobg.png" alt="Image Description" class="w-16 h-16 object-cover mx-auto"></img></td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
     </tr>      
     <tr>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center font-bold">
+        <td class="px-6 py-4 border-solid border-gray-200 text-center">
         <img src="logobg.png" alt="Image Description" class="w-16 h-16 object-cover mx-auto"></img></td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
-        <td class="px-6 py-4 border-solid border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
+        <td class="px-6 py-4 border-solid text-green-900 font-semibold border-gray-200 text-center">data</td>
     </tr>      
   
         {/* nababa pag dinagdagan ng data*/}
@@ -95,13 +95,7 @@ const userinventory = () => {
     </table>
   </div>
   
-  <div className="h-auto w-auto flex flex-row text-black justify-end font-sans font-semibold container mx-auto mt-10 items-end ">
-            <div class="w-1/6">VOUCHER</div>
-  
-  
-            <div class="w-1/6">$$$</div>
-        </div>
-        <div className="h-auto w-auto flex flex-row justify-end  container mx-auto font-sans font-semibold items-end text-black">
+        <div className="h-auto w-auto flex flex-row text-black justify-end font-sans font-semibold container mx-auto mt-10 items-end ">
             <div class="w-1/6">SUBTOTAL</div>
   
   
