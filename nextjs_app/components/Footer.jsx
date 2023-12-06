2571032b0b25c41b213eaf69948d82663d30d684
Trixia Danica Gregorio <trixxia.xx@gmail.com>
2023-12-06 12:03:26 +0800

    a

--- nextjs_app/components/Footer.jsx
+++ nextjs_app/components/Footer.jsx
@@ -1,26 +1,34 @@
 import Image from "next/image"
 import Link from "next/link"
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { AiFillInstagram } from "react-icons/ai";
+
 
 const Footer = () => {
   return (
-    <footer className="p-2 bg-green-600">
+    <footer className="p-2 bg-green">
+        
         <div className=" max-w-7xl mx-auto flex justify-between">
-            <div className="text-white text-xl font-bold">
-                <Link href="/">AgriAccess</Link>
-                <Link href="/"><Image src="/logonobg.png" alt="Logo" width={75} height={40} /> </Link>
+            <div className="text-white text-xl font-semibold mt-[50px]">
+                <Link href="/" className="font-Bree text-5xl ">AgriAccess</Link>
+                <Link href="/">
+                    <Image src="/logo-white.png" alt="Logo" width={100} height={100} 
+                    className="mt-[10px]"/> </Link>
             </div>
-            <div className="text-white text-xl font-bold">
-                <p>Contact Us</p>
-                <p>+63912345678</p>
-                <p>agriaccess@gmail.com</p>
+            <div className="text-white text-xl  mt-[60px]">
+                <p className="font-Bebas text-3xl text-AgriAccessOrange">Contact Us</p>
+                <p className="font-Mont text-xl">+63912345678</p>
+                <p className="font-Mont text-xl">agriaccess@gmail.com</p>
             </div>
         </div>
-        <hr/>
-        <div className=" max-w-7xl mx-auto flex justify-end">
+        <hr className="mt-[30px]"/>
+        <div className=" max-w-7xl mx-auto flex justify-end mt-[20px]">
             {/* TODO: Change to socmed icons */}
-            <Link href="/"><Image src="/logonobg.png" alt="Logo" width={75} height={40} /> </Link>
-            <Link href="/"><Image src="/logonobg.png" alt="Logo" width={75} height={40} /> </Link>
-            <Link href="/"><Image src="/logonobg.png" alt="Logo" width={75} height={40} /> </Link>
+
+            <Link href="https://www.facebook.com/"><FaFacebook size={35} className="mr-5"/> </Link>
+            <Link href="https://www.x.com/"><FaXTwitter size={35} className="mr-5"/> </Link>
+            <Link href="https://www.instagram.com/"><AiFillInstagram size={35} className="mr-5   "/> </Link>
         </div>
 
     </footer>
